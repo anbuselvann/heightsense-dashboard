55ff8d4d5916def1352cd3d77dec42ddadf5d76a
anbuselvann <143878150+anbuselvann@users.noreply.github.com>
2026-04-08 21:57:01 +0530

    commited

--- app.py
+++ app.py
@@ -315,4 +315,5 @@ if __name__ == '__main__':
     print("   GET  /config   — Get current config")
     print("   GET  /latest   — Get latest measurement (for dashboard)")
     print("   GET  /history  — Get measurement history")
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
